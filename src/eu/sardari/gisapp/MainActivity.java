cdf92f2ce1ed6a8ffbb1a2455be44ff829ff72b1
dasa123 <david@sardari.eu>
2014-04-14 03:55:23 +0200

    camera intent added

--- src/eu/sardari/gisapp/MainActivity.java
+++ src/eu/sardari/gisapp/MainActivity.java
@@ -1,64 +1,82 @@
 package eu.sardari.gisapp;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+import android.provider.MediaStore;
 
 public class MainActivity extends ActionBarActivity {
+
+	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+	/** Create a file Uri for saving an image or video */
+	private Uri getOutputMediaFileUri() {
+
+		if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+
+			// Store files in a private folder only accessible by the
+			// application
+			File mediaStorageDir = new File(
+					getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+					"camera");
+
+			// Create the storage directory if it does not exist
+			if (!mediaStorageDir.exists()) {
+				if (!mediaStorageDir.mkdirs()) {
+					Log.d("camera", "failed to create directory");
+					return null;
+				}
+			}
+
+			// Create a media file name
+			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+					.format(new Date());
+			File mediaFile = new File(mediaStorageDir.getPath()
+					+ File.separator + "IMG_" + timeStamp + ".jpg");
+
+			return Uri.fromFile(mediaFile);
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				// Image captured and saved to fileUri specified in the Intent
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+			}
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+		// create Intent to take a picture and return control to the calling
+		// application
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+		// create a file to save the image
+		Uri fileUri = getOutputMediaFileUri();
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		// set the image file name
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
+		// start the image capture Intent
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
 
 }
