9ba099d8b9a0ced5427a3b6e9a0f0c6df4cf83cc
dasa123 <david@sardari.eu>
2014-07-17 14:53:13 +0200

    localisation implemented

--- src/org/grid2osm/gisapp/MainActivity.java
+++ src/org/grid2osm/gisapp/MainActivity.java
@@ -1,15 +1,120 @@
 package org.grid2osm.gisapp;
 
-import android.app.Activity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+import android.location.Location;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements LocationListener,
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	// Object that holds accuracy and frequency parameters
+	private LocationRequest mLocationRequest;
+
+	// Current instantiation of the location client
+	private LocationClient mLocationClient;
+
+	/*
+	 * Called by Location Services when the request to connect the client
+	 * finishes successfully. At this point, you can request the current
+	 * location or start periodic updates
+	 */
+	@Override
+	public void onConnected(Bundle bundle) {
+
+		startPeriodicUpdates();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Create a new global location parameters object
+		mLocationRequest = LocationRequest.create();
+
+		// Set the update interval ceiling in milliseconds
+		mLocationRequest.setFastestInterval(1000);
+
+		// Set the update interval in milliseconds
+		mLocationRequest.setInterval(5000);
+
+		// Use high accuracy
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+		// Create a new location client, using the enclosing class to handle
+		// callbacks
+		mLocationClient = new LocationClient(this, this, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+
+	}
+
+	// Called when the Activity is restarted, even before it becomes visible.
+	@Override
+	protected void onStart() {
+
+		super.onStart();
+
+		/*
+		 * Connect the client. Don't re-start any requests here; instead, wait
+		 * for onResume()
+		 */
+		mLocationClient.connect();
+	}
+
+	/*
+	 * Called when the Activity is no longer visible at all. Stop updates and
+	 * disconnect.
+	 */
+	@Override
+	protected void onStop() {
+
+		// If the client is connected
+		if (mLocationClient.isConnected()) {
+			stopPeriodicUpdates();
+		}
+
+		// After disconnect() is called, the client is considered "dead".
+		mLocationClient.disconnect();
+
+		super.onStop();
+	}
+
+	/**
+	 * In response to a request to start updates, send a request to Location
+	 * Services
+	 */
+	private void startPeriodicUpdates() {
+
+		mLocationClient.requestLocationUpdates(mLocationRequest, this);
+	}
+
+	/**
+	 * In response to a request to stop updates, send a request to Location
+	 * Services
+	 */
+	private void stopPeriodicUpdates() {
+
+		mLocationClient.removeLocationUpdates(this);
+	}
 }
